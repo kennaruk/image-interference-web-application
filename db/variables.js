a28214ca51413ad4f56cbec51e070399f019d7ca
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2017-11-17 04:21:11 +0700

    mockup trial1

--- db/variables.js
+++ db/variables.js
@@ -1,4 +1,9 @@
 exports.images = [
+    ["/images/questions/01/test/01.jpg", "/images/questions/01/test/02.jpg", "/images/questions/01/test/03.jpg",
+    "/images/questions/01/test/04.jpg", "/images/questions/01/test/05.jpg", "/images/questions/01/test/06.jpg",
+    "/images/questions/01/test/07.jpg", "/images/questions/01/test/08.jpg", "/images/questions/01/test/09.jpg",
+    "/images/questions/01/test/10.jpg"],
+
     ["https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
     "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg"],
     ["https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
@@ -6,6 +11,8 @@ exports.images = [
 ]
 
 exports.answers_images = [
+    ["/images/questions/01/test/11.jpg", "/images/questions/01/test/04.jpg", "/images/questions/01/test/12.jpg", "/images/questions/01/test/13.jpg"],
+
     ["https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
     "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
     "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
@@ -16,4 +23,4 @@ exports.answers_images = [
     "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg"]
 ]
 
-exports.answers = [1, 1, 2, 4];
+exports.answers = [2, 1, 2, 4, 1];
