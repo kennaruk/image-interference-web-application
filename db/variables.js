224cd1f57a456bb2b6f3bcfeae672894f3c38342
Ken Nitipat <k-e-n_ken@hotmail.com>
2017-11-17 13:04:33 +0700

    images 01 02 03 05 added

--- db/variables.js
+++ db/variables.js
@@ -4,15 +4,34 @@ exports.images = [
     "/images/questions/01/test/07.jpg", "/images/questions/01/test/08.jpg", "/images/questions/01/test/09.jpg",
     "/images/questions/01/test/10.jpg"],
 
+    ["/images/questions/02/01.JPG", "/images/questions/02/02.JPG", "/images/questions/02/03.JPG",
+    "/images/questions/02/04.JPG", "/images/questions/02/05.JPG", "/images/questions/02/06.JPG",
+    "/images/questions/02/07.JPG", "/images/questions/02/08.JPG", "/images/questions/02/09.JPG",
+    "/images/questions/02/10.JPG"],
+
+    ["/images/questions/03/01.JPG", "/images/questions/03/02.JPG", "/images/questions/03/03.JPG",
+     "/images/questions/03/04.JPG", "/images/questions/03/05.JPG", "/images/questions/03/06.JPG",
+     "/images/questions/03/07.JPG", "/images/questions/03/08.JPG", "/images/questions/03/09.JPG",
+     "/images/questions/03/10.JPG"],
+
     ["https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
     "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg"],
     ["https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
     "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg"]
+
+    ["/images/questions/05/01.jpg", "/images/questions/05/02.jpg", "/images/questions/05/03.jpg",
+     "/images/questions/05/04.jpg", "/images/questions/05/05.jpg", "/images/questions/05/06.jpg",
+     "/images/questions/05/07.jpg", "/images/questions/05/08.jpg", "/images/questions/05/09.jpg",
+     "/images/questions/05/10.jpg"],
 ]
 
 exports.answers_images = [
     ["/images/questions/01/test/11.jpg", "/images/questions/01/test/04.jpg", "/images/questions/01/test/12.jpg", "/images/questions/01/test/13.jpg"],
 
+    ["/images/questions/02/04.JPG", "/images/questions/02/11.JPG", "/images/questions/02/12.JPG", "/images/questions/02/13.JPG"],
+
+    ["/images/questions/03/12.JPG", "/images/questions/03/11.JPG", "/images/questions/03/04.JPG", "/images/questions/03/13.JPG"],
+
     ["https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
     "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
     "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
@@ -21,6 +40,9 @@ exports.answers_images = [
     "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
     "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
     "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg"]
+
+    ["/images/questions/05/11.jpg", "/images/questions/05/13.jpg", "/images/questions/05/12.jpg", "/images/questions/05/08.jpg"],
+    
 ]
 
-exports.answers = [2, 1, 2, 4, 1];
+exports.answers = [2, 1, 3, 4, 4];
